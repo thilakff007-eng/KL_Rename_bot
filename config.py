693aa679474395ad74af120e70e7d1126d626fd6
TDBotDev <tdbotdev1@gmail.com>
2026-07-14 09:35:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ import re, os, time
 id_pattern = re.compile(r'^.\d+$') 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "31819748"))
-    API_HASH = os.environ.get("API_HASH", "1187ad5f9b4ceac7fa9cfc492040dfee")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8072657598:AAHadERTmGpXrzbgMoKthoC-NhA6ueBiEE0") 
+    API_ID = int(os.environ.get("API_ID", "31819748")) # Teligram Api id : https://youtu.be/L5DDah6WhIM
+    API_HASH = os.environ.get("API_HASH", "1187ad5f9b4ceac7fa9cfc492040dfee") #Teligram Api hash : https://youtu.be/L5DDah6WhIM
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8072657598:AAHadERTmGpXrzbgMoKthoC-NhA6ueBiEE0") #Teligram Bot Token: https://youtu.be/tqV2jH5YWFY
     BOT = None
 
     # premium account string session required 😢 
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     
     # database config
-    DB_NAME = os.environ.get("DB_NAME","tdanimehub")     
+    DB_NAME = os.environ.get("DB_NAME","tdanimehub") # Mango db name : https://youtu.be/Cp_p9DtrWAw
     DB_URL = os.environ.get("DB_URL","mongodb+srv://tdanimehub_db_user:cPdMT253KSZpE11Z@helper.wallqjf.mongodb.net/?retryWrites=true&w=majority&appName=Helper")
  
     # other configs
